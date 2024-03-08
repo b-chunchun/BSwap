889360df014816200585bcab8dd03aa658e842cf
zhichunchun <zhichunchun@iqingmiao.com>
2024-03-08 11:28:32 +0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/BSwap/precache-manifest.355669e4e5c2b162c4825c3154f9f376.js"
+  "/BSwap/precache-manifest.d15a9097d8f88db831d029b2a7b978a0.js"
 );
 
 self.addEventListener('message', (event) => {
